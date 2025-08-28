daf2851cf1e0f3b0f2008a9fd7e447baab76f938
Mateus Melo <mateus@mateusmelo.com>
2025-08-27 21:25:52 -0300

    Update settings to use env values

--- pliromi/settings.py
+++ pliromi/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!%(vll_xp^mc3%fk!6q*^ikizlo6&m2)5*fe)+3xl*c@eedw)f'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
